910e97408c73236b1682155f4b1e22116823ba5a
EnriqueTests <enriquenada@outlook.es>
2021-07-21 18:13:26 -0500

    Modulo Fs en modo sincrono

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const path = require('path')
+const { readFileSync, writeFileSync } = require('fs')
 
-console.log(path.sep)
+const first = readFileSync('./content/first.txt', 'utf8')
+const second = readFileSync('./content/second.txt', 'utf8')
 
-const filePath = path.join('/content', 'subfolder', 'test.txt')
-console.log(filePath)
-
-const base = path.basename(filePath)
-console.log(base)
-
-const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
-console.log(absolute)
+writeFileSync(
+    './content/result-sync.txt',
+    `Here is the result : ${first}, ${second}`,
+    { flag: 'a' }
+)
