1a945d95bcdcf3e4ed959f63a416230423b4db9c
EnriqueTests <enriquenada@outlook.es>
2021-07-21 19:23:07 -0500

    Mas configuraciones del modulo http

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    res.write('Welcome to our hme page')
-    res.end()
+    if(req.url === '/') {
+        res.end('Welcome to our page')
+    }
+    if(req.url === '/about') {
+        res.end('Here is our short history')
+    }
+    res.end(`
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page </p>
+    <a href="/">back home</a>
+    `)
+    
 })
 
 server.listen(5000)
