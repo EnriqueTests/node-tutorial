8dec1de5e84942a03d38084dc19e6d99a98688ff
EnriqueTests <enriquenada@outlook.es>
2021-07-22 12:28:47 -0500

    Patrones asincronos - Opcion de nodo nativa

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-const { readFile } = require('fs')
-
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, 'utf8', (err, data) => {
-      if(err) {
-        reject(err)
-      } else {
-        resolve(data)
-      }
-    })
-  })
-}
+const { readFile, writeFile } = require('fs').promises
 
 const start = async () => {
   try {
-    const first = await getText('./content/first.txt')
-    const second = await getText('./content/second.txt')
+    const first = await readFilePromise('./content/first.txt', 'utf8')
+    const second = await readFilePromise('./content/second.txt', 'utf8')
+    await writeFilePromise(
+      './content/result-mind-grenade.txt',
+      `THIS IS AWESOME : ${first} ${second}`,
+      { flag: 'a'}
+    )
     console.log(first, second)
   } catch (error) {
     console.log(error)
